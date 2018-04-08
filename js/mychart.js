138853c14b358e9a2c8b6416c63273ff84df376e
prismistim <prismistim@hotmail.com>
2018-04-09 02:58:34 +0900

    レイアウト制作途中

--- js/mychart.js
+++ js/mychart.js
@@ -60,19 +60,96 @@
 // }
 
 // main();
-function drawChart() {  
-  var ctx = document.getElementById('Gender').getContext('2d');
-  var Class = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      labels: ["1年", "2年", "3年", "4年", "5年"],
-      datasets: [{
-        data: [45, 21, 28, 21, 5],
-        backgroundColor: ["#7EFFD6", "7AE86A", "FFF93A", "E8B542", "FF8C53"]
-      }]
+var ctx = $('#Class').get(0).getContext('2d');
+var Class = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ["1年", "2年", "3年", "4年", "5年"],
+    datasets: [{
+      backgroundColor: ["#7EFFD6", "#7AE86A", "#FFF93A", "#E8B542", "#FF8C53"],
+      data: [45, 21, 28, 21, 5]
+    }]
+  },
+  options: {
+    maintainAspectRatio: false,
+  }
+});
+
+var ctx1 = $('#Gender').get(0).getContext('2d');
+var Class = new Chart(ctx1, {
+  type: 'pie',
+  data: {
+    labels: ["男子", "女子"],
+    datasets: [{
+      backgroundColor: ["#6666F3", "#F36666"],
+      data: [101, 19]
+    }]
+  },
+  options: {
+    maintainAspectRatio: false,
+  }
+});
+
+var ctx2 = $('#Course').get(0).getContext('2d');
+var Class = new Chart(ctx2, {
+  type: 'pie',
+  data: {
+    labels: ["電気電子工学コース", "情報工学コース", "迷っている"],
+    datasets: [{
+      backgroundColor: ["#A3A322", "#4ac9f2", "#555555"],
+      data: [37, 58, 25]
+    }]
+  },
+  options: {
+    maintainAspectRatio: false,
+  }
+});
+
+var ctx3 = $('#License').get(0).getContext('2d');
+var Class = new Chart(ctx3, {
+  type: 'bar',
+  data: {
+    labels: ["英検", "数検", "漢検", "TOEIC(500点以上)", "アマチュア無線", "基本情報技術者(FE)", "危険物取扱者(乙4等)"],
+    datasets: [{
+      label: "取得人数",
+      backgroundColor: "#A3A322",
+      data: [59, 43, 40, 17, 22, 18, 15]
+    }]
+  },
+  options: {
+    maintainAspectRatio: false,
+    scales: {                          //軸設定
+      yAxes: [{                      //y軸設定
+          ticks: {                      //最大値最小値設定
+              min: 0,                   //最小値
+              max: 60,                  //最大値
+              stepSize: 10               //軸間隔
+          },
+      }],
     }
-  });
-}
+  }
+});
+
+
+// var ctx1 = document.getElementById("Gender").getContext('2d');
+// var Gender = new Chart(ctx1, {
+//   type: 'pie',
+//   data: {
+//     labels: ["M", "T", "W", "T", "F", "S", "S"],
+//     datasets: [{
+//       backgroundColor: [
+//         "#2ecc71",
+//         "#3498db",
+//         "#95a5a6",
+//         "#9b59b6",
+//         "#f1c40f",
+//         "#e74c3c",
+//         "#34495e"
+//       ],
+//       data: [12, 19, 3, 17, 28, 24, 7]
+//     }]
+//   }
+// });
 
 // var ctx2 = $("#Gender").getContext('2d');
 //   var gender = new Chart(ctx2, {
